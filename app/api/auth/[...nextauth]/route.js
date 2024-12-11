01358b5b40b13d5b3f7f8787eac675152a3cb1dd
Vikhyatz <vikhyatgupta61@gmail.com>
2024-12-12 00:22:29 +0530

    authentication

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -9,20 +9,15 @@ import GithubProvider from 'next-auth/providers/github'
 const handler = NextAuth({
     providers: [
         GithubProvider({
-            // clientId: process.env.GITHUB_ID,
-            // clientSecret: process.env.GITHUB_SECRET,
-            clientId: 'Ov23lib4hasb6fSETsX9',
-            clientSecret: '808302495325a9db5bcdab867afbb3907e02cbef',
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
         }),
         GoogleProvider({
-            clientId: '631324538908-3dk8i5q6797k1d19t6ot4acgl3ongbhd.apps.googleusercontent.com',
-            clientSecret: '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'
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET
         }),
     ],
 
 })
-
-// google client id - 631324538908-3dk8i5q6797k1d19t6ot4acgl3ongbhd.apps.googleusercontent.com
-// google client secret - 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
 
 export { handler as GET, handler as POST }
